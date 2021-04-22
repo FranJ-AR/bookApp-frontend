c7534584002dc6edc1c34787ea70a410302850b0
FranJ-AR <fraabro.inf@gmail.com>
2021-04-22 18:26:56 +0200

    Added more sections to the website (some of them with default text)

---
+++ src/app/online-chat/online-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-online-chat',
+  templateUrl: './online-chat.component.html',
+  styleUrls: ['./online-chat.component.scss']
+})
+export class OnlineChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
