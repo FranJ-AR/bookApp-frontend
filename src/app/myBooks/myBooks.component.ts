ef040c468654307120e908682e9b65b9cf913397
FranJ-AR <fraabro.inf@gmail.com>
2021-04-26 13:51:42 +0200

    Partially added book details to my books

--- src/app/myBooks/myBooks.component.ts
+++ src/app/myBooks/myBooks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Book } from 'src/Book';
 import { Loan } from 'src/Loan';
+import { BookUserStatus } from '../BookUserStatus';
 import { LoanService } from '../loan.service';
 import { Reservation } from '../Reservation';
 import { ReservationService } from '../reservation.service';
@@ -18,6 +20,12 @@ export class MyBooksComponent implements OnInit, OnDestroy {
   loans: Loan[] = []
   reservations: Reservation[] = [];
   userSubscription: Subscription | null = null;
+  
+  books:Book[] = [];
+
+  showDetails:boolean = false;
+
+  indexArrayBooks: number = -1;
 
   constructor(private userAuthService:UserAuthService, 
     private loanService:LoanService, private reservationService:ReservationService) { }
@@ -96,6 +104,83 @@ export class MyBooksComponent implements OnInit, OnDestroy {
       this.reservations = reservations;
 
     })
+
+  }
+
+  private findBookIdByBookIndex(id: number): void {
+
+    this.indexArrayBooks = -1;
+
+    /* checks every value until from index 0 to size-1 until 
+       one matches the condition and stops */
+
+    this.books.some((element, index) => {
+
+      if (element.id === id) {
+
+        this.indexArrayBooks = index;
+
+        return true;
+
+      }
+
+      return false;
+
+    })
+
+  }
+
+  showDetailsBookLoaned(bookId:number):void{
+
+    this.loansToBooks();
+
+    this.showDetails = true;
+
+    this.findBookIdByBookIndex(bookId);
+    
+  }
+
+  showDetailsBookReservated(bookId:number):void{
+
+    this.reservationsToBooks();
+
+    this.showDetails = true;
+
+    this.findBookIdByBookIndex(bookId);
+    
+  }
+
+  private loansToBooks():void{
+
+    this.books = [];
+
+    this.loans.map( (loan) => {
+
+      loan.book.userStatus = BookUserStatus.Loaned;
+
+      this.books.push(loan.book);
+
+    })
+  }
+
+  private reservationsToBooks():void{
+
+    this.books = [];
+
+    this.reservations.map( (loan) => {
+
+      loan.book.userStatus = BookUserStatus.Reservated;
+
+      this.books.push(loan.book);
+
+    })
+
+  }
+
+  // has a placeholder value, required by Angular but not needed
+  showDetailsOnClose(newValue:any):void{
+
+    this.showDetails = false;
 
   }
 
