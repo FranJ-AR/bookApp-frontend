3e4a25e9a8aa30147a7d1f4c44b0ceb56e106947
FranJ-AR <fraabro.inf@gmail.com>
2021-04-28 12:00:28 +0200

    Removed unneeded clases and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrivateSectionComponent } from './private-section/private-section.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AuthAlreadyLogged } from './authAlreadyLogged.guard';
 import { IndexComponent } from './index/index.component';
-import { PublicSectionRoutingModule } from './public-section/public-section-routing.module';
 import { AuthInterceptorService } from './auth-interceptor.service';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AuthComponent } from './auth/auth.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { MytestsComponent } from './mytests/mytests.component';
 import { MyBooksComponent } from './myBooks/myBooks.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { FaqComponent } from './faq/faq.component';
@@ -28,12 +25,10 @@ import { OnlineChatComponent } from './online-chat/online-chat.component';
   declarations: [
     AppComponent,
     AuthComponent,
-    PrivateSectionComponent,
     IndexComponent,
     FooterComponent,
     NavbarComponent,
     BookDetailsComponent,
-    MytestsComponent,
     MyBooksComponent,
     NotfoundComponent,
     FaqComponent,
@@ -47,7 +42,6 @@ import { OnlineChatComponent } from './online-chat/online-chat.component';
     NgbModule,
     HttpClientModule,
     FormsModule,
-    PublicSectionRoutingModule,
     FontAwesomeModule,
     
   ],
